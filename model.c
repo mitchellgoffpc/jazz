8c38f6530b3b735848df8365dbc7525a0603d7e4
Mitchell Goff <mitchellgoffpc@gmail.com>
2023-08-09 23:19:39 -0700

    wrote forward pass, probably still a lot of bugs

--- model.c
+++ model.c
@@ -14,6 +14,8 @@
 typedef struct {
   float* weight;
   float* bias;
+  int in_size;
+  int out_size;
 } Linear;
 
 typedef struct {
@@ -27,9 +29,11 @@ typedef struct {
 } GPT2;
 
 typedef struct {
+  float* past;
   float* x;
   float* ln1;
   float* qkv;
+  float* attn;
   float* proj;
   float* ln2;
   float* fc1;
@@ -43,6 +47,8 @@ typedef struct {
 void init_linear(Linear* layer, int in_size, int out_size) {
   layer->weight = malloc(in_size * out_size * sizeof(float));
   layer->bias = malloc(out_size * sizeof(float));
+  layer->in_size = in_size;
+  layer->out_size = out_size;
 }
 
 void init_model(GPT2* model) {
@@ -59,6 +65,7 @@ void init_state(State* state) {
   state->x = malloc(EMBED_SIZE * sizeof(float));
   state->ln1 = malloc(EMBED_SIZE * sizeof(float));
   state->qkv = malloc(3*EMBED_SIZE * sizeof(float));
+  state->attn = malloc(CONTEXT_SIZE * sizeof(float));
   state->proj = malloc(EMBED_SIZE * sizeof(float));
   state->ln2 = malloc(EMBED_SIZE * sizeof(float));
   state->fc1 = malloc(EMBED_SIZE * sizeof(float));
@@ -69,30 +76,54 @@ void init_state(State* state) {
 
 // Helper Functions
 
-float* add(float* out, float* x, float* y) {
-  for (int i = 0; i < EMBED_SIZE; i++) {
-    out[i] = x[i] + y[i];
+#define ELEMENTWISE(i, n, out, expr) ({ for (int (i) = 0; (i) < (n); i++) { out[i] = (expr); } out; })
+#define REDUCE(i, n, acc, expr) ({ float acc = 0; for (int (i) = 0; (i) < (n); i++) { acc = (expr); } acc; })
+
+float* add(float* out, float* x, float* y, int n) {
+  return ELEMENTWISE(i, n, out, x[i] + y[i]);
+}
+float* shift(float* x, float c, int n) {
+  return ELEMENTWISE(i, n, x, c + x[i]);
+}
+float* scale(float* x, float c, int n) {
+  return ELEMENTWISE(i, n, x, c * x[i]);
+}
+float* vexp(float* x, int n) {
+  return ELEMENTWISE(i, n, x, (float) exp(x[i]));
+}
+float* gelu(float* x, int n) {
+  float sqrt2overPI = sqrt(2 / M_PI);
+  return ELEMENTWISE(i, n, x, 0.5 * x[i] * (1 + tanh(sqrt2overPI * (x[i] + 0.044715 * x[i]*x[i]*x[i]))));
+}
+
+float sum(float* x, int n) {
+  return REDUCE(i, n, acc, acc + x[i]);
+}
+float max(float* x, int n) {
+  return REDUCE(i, n, acc, x[i] > acc ? x[i] : acc);
+}
+
+float* matmul(float* out, float* A, float* X, int n_cols, int n_rows) {
+  for (int row = 0; row < n_rows; row++) {
+    out[row] = REDUCE(col, n_cols, acc, acc + A[row*n_cols + col] * X[col]);
   }
   return out;
 }
 
-float* matmul(float* A, float* B) {
-  return NULL;
+float* linear(float* out, Linear* layer, float* data) {
+  out = matmul(out, layer->weight, data, layer->in_size, layer->out_size);
+  out = add(out, layer->bias, out, layer->out_size);
+  return out;
 }
 
 float* embedding(float* data, int idx) {
   return &data[idx*EMBED_SIZE];
 }
 
-float* linear(float* out, Linear* layer, float* data) {
-  return data;
-}
-
-float* gelu(float* x) {
-  float sqrt2overPI = sqrt(2 / M_PI);
-  for (int i = 0; i < EMBED_SIZE; i++) {
-    x[i] = 0.5 * x[i] * (1 + tanh(sqrt2overPI * (x[i] + 0.044715 * x[i]*x[i]*x[i])));
-  }
+float* softmax(float* x, int n) {
+  x = shift(x, -max(x, n), n);
+  x = vexp(x, n);
+  x = scale(x, 1 / sum(x, n), n);
   return x;
 }
 
@@ -113,22 +144,30 @@ float* norm(float* x) {
 
 // Forward pass
 
-float* attention(GPT2* model, State* state, int layer, float* x) {
+float* attention(GPT2* model, State* state, float* past, int past_len, int layer, float* x) {
+  float* qkv, *attn, *q, *k, *v;
+  qkv = linear(state->qkv, &model->qkv, x);
+  q = &qkv[0], k = &qkv[EMBED_SIZE], v = &qkv[2*EMBED_SIZE];
+  attn = matmul(state->attn, k,  q, EMBED_SIZE, past_len);
+  attn = scale(attn, (1.0 / sqrt(EMBED_SIZE)), past_len);
+  attn = softmax(attn, past_len);
+  x = matmul(state->attn, v, attn, past_len, EMBED_SIZE);
+  x = linear(state->proj, &model->proj, x);
   return x;
 }
 
 float* mlp(GPT2* model, State* state, int layer, float* x) {
   x = linear(state->fc1, &model->fc1, x);
-  x = linear(state->fc2, &model->fc2, gelu(x));
+  x = linear(state->fc2, &model->fc2, gelu(x, EMBED_SIZE));
   return x;
 }
 
-float* eval(GPT2* model, State* state, int token, int context_len) {
+float* gpt(GPT2* model, State* state, float* past, int past_len, int token) {
   float* x;
-  x = add(state->x, embedding(model->embed_tokens, token), embedding(model->embed_pos, context_len));
+  x = add(state->x, embedding(model->embed_tokens, token), embedding(model->embed_pos, past_len), EMBED_SIZE);
   for (int i = 0; i < NUM_LAYERS; i++) {
-    x = add(state->x, x, attention(model, state, i, norm(x)));
-    x = add(state->x, x, mlp(model, state, i, norm(x)));
+    x = add(state->x, x, attention(model, state, past, past_len, i, norm(x)), EMBED_SIZE);
+    x = add(state->x, x, mlp(model, state, i, norm(x)), EMBED_SIZE);
   }
   return x;
 }
@@ -140,11 +179,12 @@ int main() {
   // deserialize()
   GPT2* model = malloc(sizeof(GPT2));
   State* state = malloc(sizeof(State));
+  float* past = malloc(2 * NUM_LAYERS * CONTEXT_SIZE * EMBED_SIZE * sizeof(float));
 
   init_model(model);
   init_state(state);
 
   int context[512] = {0};
-  eval(model, state, 0, 0);
+  gpt(model, state, past, 0, 0);
   printf("DONE\n");
 }
