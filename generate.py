b7b685ff6c3df360f00b3181b89babf29c8e7de4
mitchellgoffpc <mitchellgoffpc@gmail.com>
2024-07-26 00:29:31 -0700

    Change to 4-space indentation

--- generate.py
+++ generate.py
@@ -9,121 +9,123 @@ import tiktoken
 from tqdm import tqdm, trange
 from models.gpt import GPT, GPTConfig
 
-def load_checkpoint(weights_url, checkpoint_fn):
-  tmp_checkpoint_fn = f'{checkpoint_fn}.tmp'
-  if not os.path.exists(checkpoint_fn):
-    r = requests.get(weights_url, stream=True)
-    file_size = int(r.headers['content-length'])
-    chunk_size = 1000  # 1k for chunk_size, since Ethernet packet size is around 1500 bytes
-    with open(tmp_checkpoint_fn, 'wb') as f:
-      with tqdm(ncols=100, desc="Fetching " + weights_url, total=file_size, unit_scale=True) as pbar:
-        for chunk in r.iter_content(chunk_size=chunk_size):
-          f.write(chunk)
-          pbar.update(chunk_size)
-    os.rename(tmp_checkpoint_fn, checkpoint_fn)
+CONFIGS = {
+    'gpt2': GPTConfig(num_layers=12, num_heads=12, embed_size=768),
+    'gpt2-medium': GPTConfig(num_layers=24, num_heads=16, embed_size=1024),
+    'gpt2-large': GPTConfig(num_layers=36, num_heads=20, embed_size=1280),
+    'gpt2-xl': GPTConfig(num_layers=48, num_heads=25, embed_size=1600)}
 
-  with open(checkpoint_fn, 'rb') as f:
-    header_len, = struct.unpack('<Q', f.read(8))
-    metadata = json.loads(f.read(header_len))
-    tensor_data = bytearray(f.read()).copy()
-    state_dict = {}
-    for k,v in metadata.items():
-      if k != '__metadata__':
-        s, e = v['data_offsets']
-        state_dict[k] = torch.frombuffer(tensor_data[s:e], dtype=torch.float32).view(v['shape'])
-    return state_dict
+
+def load_checkpoint(weights_url, checkpoint_fn):
+    tmp_checkpoint_fn = f'{checkpoint_fn}.tmp'
+    if not os.path.exists(checkpoint_fn):
+        r = requests.get(weights_url, stream=True)
+        file_size = int(r.headers['content-length'])
+        chunk_size = 128 * 1000  # 1k for chunk_size, since Ethernet packet size is around 1500 bytes
+        with open(tmp_checkpoint_fn, 'wb') as f:
+            with tqdm(desc="Fetching " + weights_url, total=file_size, unit_scale=True) as pbar:
+                for chunk in r.iter_content(chunk_size=chunk_size):
+                    f.write(chunk)
+                    pbar.update(chunk_size)
+        os.rename(tmp_checkpoint_fn, checkpoint_fn)
+
+    with open(checkpoint_fn, 'rb') as f:
+        header_len, = struct.unpack('<Q', f.read(8))
+        metadata = json.loads(f.read(header_len))
+        tensor_data = bytearray(f.read()).copy()
+        state_dict = {}
+        for k,v in metadata.items():
+            if k != '__metadata__':
+                s, e = v['data_offsets']
+                state_dict[k] = torch.frombuffer(tensor_data[s:e], dtype=torch.float32).view(v['shape'])
+        return state_dict
 
 
 def fix_state_dict(state_dict):
-  replacements = {
-    'h.': 'blocks.',
-    'wte.': 'embed_tokens.',
-    'wpe.': 'embed_pos.',
-    'attn.c_attn': 'attn.qkv',
-    'attn.c_proj': 'attn.proj',
-    'mlp.c_fc': 'mlp.fc1',
-    'mlp.c_proj': 'mlp.fc2',
-    'ln_1.': 'ln1.',
-    'ln_2.': 'ln2.',
-    'ln_f.': 'ln.'}
-  linears = ['attn.qkv.weight', 'attn.proj.weight', 'mlp.fc1.weight', 'mlp.fc2.weight']
-  biases = ['attn.bias', 'attn.masked_bias']
+    replacements = {
+        'h.': 'blocks.',
+        'wte.': 'embed_tokens.',
+        'wpe.': 'embed_pos.',
+        'attn.c_attn': 'attn.qkv',
+        'attn.c_proj': 'attn.proj',
+        'mlp.c_fc': 'mlp.fc1',
+        'mlp.c_proj': 'mlp.fc2',
+        'ln_1.': 'ln1.',
+        'ln_2.': 'ln2.',
+        'ln_f.': 'ln.'}
+    linears = ['attn.qkv.weight', 'attn.proj.weight', 'mlp.fc1.weight', 'mlp.fc2.weight']
+    biases = ['attn.bias', 'attn.masked_bias']
 
-  for src,dst in replacements.items():
-    state_dict = {k.replace(src, dst): v for k,v in state_dict.items()}
-  state_dict = {k:v for k,v in state_dict.items() if not any(x in k for x in biases)}
-  state_dict = {k: v.transpose(-1, -2) if any(x in k for x in linears) else v for k,v in state_dict.items()}
-  state_dict['fc_out.weight'] = state_dict['embed_tokens.weight']
-  return state_dict
+    for src,dst in replacements.items():
+        state_dict = {k.replace(src, dst): v for k,v in state_dict.items()}
+    state_dict = {k:v for k,v in state_dict.items() if not any(x in k for x in biases)}
+    state_dict = {k: v.transpose(-1, -2) if any(x in k for x in linears) else v for k,v in state_dict.items()}
+    state_dict['fc_out.weight'] = state_dict['embed_tokens.weight']
+    return state_dict
 
 
 def serialize_model(config, state_dict, output_fn):
-  def serialize_layer(data):
-    f.write(struct.pack('<Q', data.nelement()))
-    f.write(data.numpy().tobytes())
+    def serialize_layer(data):
+        f.write(struct.pack('<Q', data.nelement()))
+        f.write(data.numpy().tobytes())
 
-  def serialize_block(key):
-    serialize_layer(torch.stack([state_dict[f'blocks.{i}.{key}.weight'] for i in range(config.num_layers)], dim=0))
-    serialize_layer(torch.stack([state_dict[f'blocks.{i}.{key}.bias'] for i in range(config.num_layers)], dim=0))
+    def serialize_block(key):
+        serialize_layer(torch.stack([state_dict[f'blocks.{i}.{key}.weight'] for i in range(config.num_layers)], dim=0))
+        serialize_layer(torch.stack([state_dict[f'blocks.{i}.{key}.bias'] for i in range(config.num_layers)], dim=0))
 
-  with open(output_fn, 'wb') as f:
-    f.write(struct.pack('<Q', len(state_dict)))
-    f.write(struct.pack('<5Q', config.num_layers, config.num_heads, config.embed_size, config.vocab_size, config.context_size))
+    with open(output_fn, 'wb') as f:
+        f.write(struct.pack('<Q', len(state_dict)))
+        f.write(struct.pack('<5Q', config.num_layers, config.num_heads, config.embed_size, config.vocab_size, config.context_size))
 
-    serialize_layer(state_dict['embed_tokens.weight'])
-    serialize_layer(state_dict['embed_pos.weight'])
-    serialize_block('ln1')
-    serialize_block('ln2')
-    serialize_block('attn.qkv')
-    serialize_block('attn.proj')
-    serialize_block('mlp.fc1')
-    serialize_block('mlp.fc2')
-    serialize_layer(state_dict['ln.weight'])
-    serialize_layer(state_dict['ln.bias'])
-    serialize_layer(state_dict['fc_out.weight'])
+        serialize_layer(state_dict['embed_tokens.weight'])
+        serialize_layer(state_dict['embed_pos.weight'])
+        serialize_block('ln1')
+        serialize_block('ln2')
+        serialize_block('attn.qkv')
+        serialize_block('attn.proj')
+        serialize_block('mlp.fc1')
+        serialize_block('mlp.fc2')
+        serialize_layer(state_dict['ln.weight'])
+        serialize_layer(state_dict['ln.bias'])
+        serialize_layer(state_dict['fc_out.weight'])
 
-
-
-CONFIGS = {
-  'gpt2': GPTConfig(num_layers=12, num_heads=12, embed_size=768),
-  'gpt2-medium': GPTConfig(num_layers=24, num_heads=16, embed_size=1024)}
 
 if __name__ == '__main__':
-  model = sys.argv[1]
-  assert model in CONFIGS
+    model = sys.argv[1]
+    assert model in CONFIGS
 
-  # Load weights
-  weights_url = f'https://huggingface.co/{model}/resolve/main/model.safetensors'
-  checkpoint_fn = f'/tmp/{model}.safetensors'
-  state_dict = load_checkpoint(weights_url, checkpoint_fn)
-  state_dict = fix_state_dict(state_dict)
+    # Load weights
+    weights_url = f'https://huggingface.co/{model}/resolve/main/model.safetensors'
+    checkpoint_fn = f'/tmp/{model}.safetensors'
+    state_dict = load_checkpoint(weights_url, checkpoint_fn)
+    state_dict = fix_state_dict(state_dict)
 
-  # Create the model
-  device = 'cpu'
-  config = CONFIGS[model]
-  model = GPT(config).to(device)
-  model.load_state_dict(state_dict)
-  tokenizer = tiktoken.get_encoding("gpt2")
+    # Create the model
+    device = 'cpu'
+    config = CONFIGS[model]
+    model = GPT(config).to(device)
+    model.load_state_dict(state_dict)
+    tokenizer = tiktoken.get_encoding("gpt2")
 
-  # Run sanity checks
-  prompt = "The capital of Germany is Berlin. The capital of France is"
-  context = torch.tensor(tokenizer.encode(prompt))[None].to(device)
-  result = model.generate(context, num_tokens=2, top_k=10, temperature=1e-6)
-  assert tokenizer.decode(result[0].tolist()[-1:]) == " Paris"
+    # Run sanity checks
+    prompt = "The capital of Germany is Berlin. The capital of France is"
+    context = torch.tensor(tokenizer.encode(prompt))[None].to(device)
+    result = model.generate(context, num_tokens=2, top_k=10, temperature=1e-6)
+    assert tokenizer.decode(result[0].tolist()[-1:]) == " Paris"
 
-  prompt = "Hi my name is Chris but I have been with Nokia for many years."
-  context = torch.tensor(tokenizer.encode(prompt))[None].to(device)
-  result = model.generate(context, num_tokens=50, top_k=10)
-  print(f"Prompt:    ", prompt)
-  print(f"Completion:", tokenizer.decode(result[0].tolist()))
+    prompt = "Hi my name is Chris but I have been with Nokia for many years."
+    context = torch.tensor(tokenizer.encode(prompt))[None].to(device)
+    result = model.generate(context, num_tokens=50, top_k=10)
+    print(f"Prompt:    ", prompt)
+    print(f"Completion:", tokenizer.decode(result[0].tolist()))
 
-  # Serialize weights
-  print("Serializing weights...")
-  serialize_model(config, state_dict, '/tmp/weights.bin')
-  print("Done serializing")
+    # Serialize weights
+    print("Serializing weights...")
+    serialize_model(config, state_dict, '/tmp/weights.bin')
+    print("Done serializing")
 
-  # Benchmark
-  large_context = torch.randint(0, 50257, size=(1, 1024)).to(device)
-  with torch.no_grad():
-    for i in trange(1024, desc="benchmarking bs=1, seqlen=1"):
-      data = model(large_context[:,i:i+1])
+    # Benchmark
+    large_context = torch.randint(0, 50257, size=(1, 1024)).to(device)
+    with torch.no_grad():
+        for i in trange(1024, desc="benchmarking bs=1, seqlen=1"):
+            data = model(large_context[:,i:i+1])
--- models/gpt.py
+++ models/gpt.py
@@ -5,107 +5,107 @@ from dataclasses import dataclass
 
 @dataclass
 class GPTConfig:
-  num_layers: int = 12
-  num_heads: int = 12
-  embed_size: int = 768
-  vocab_size: int = 50257
-  context_size: int = 1024
-  dropout: float = 0.0
-  bias: bool = True
+    num_layers: int = 12
+    num_heads: int = 12
+    embed_size: int = 768
+    vocab_size: int = 50257
+    context_size: int = 1024
+    dropout: float = 0.0
+    bias: bool = True
 
 
 class LayerNorm(nn.Module):
-  def __init__(self, n, bias):
-    super().__init__()
-    self.weight = nn.Parameter(torch.ones(n))
-    self.bias = nn.Parameter(torch.zeros(n)) if bias else None
+    def __init__(self, n, bias):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(n))
+        self.bias = nn.Parameter(torch.zeros(n)) if bias else None
 
-  def forward(self, x):
-    return F.layer_norm(x, self.weight.shape, self.weight, self.bias, 1e-5)
+    def forward(self, x):
+        return F.layer_norm(x, self.weight.shape, self.weight, self.bias, 1e-5)
 
 class Attention(nn.Module):
-  def __init__(self, config):
-    super().__init__()
-    self.config = config
-    self.qkv = nn.Linear(config.embed_size, config.embed_size*3, bias=config.bias)
-    self.proj = nn.Linear(config.embed_size, config.embed_size, bias=config.bias)
-    self.dropout = nn.Dropout(config.dropout)
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.qkv = nn.Linear(config.embed_size, config.embed_size*3, bias=config.bias)
+        self.proj = nn.Linear(config.embed_size, config.embed_size, bias=config.bias)
+        self.dropout = nn.Dropout(config.dropout)
 
-  def forward(self, x, past, past_len):
-    B, T, C = x.shape
-    H = self.config.num_heads
-    q, k, v = self.qkv(x).view(B, T, 3, H, C // H).transpose(1, 3).unbind(dim=2)  # B, H, T, C // H
-    if past is not None:  # write KV into the buffer
-      assert past_len == 0 or T == 1  # since is_causal has to be disabled
-      past[:, :, 0, past_len:past_len+T], past[:, :, 1, past_len:past_len+T] = k, v
-      k, v = past[:, :, 0, :past_len+T], past[:, :, 1, :past_len+T]
+    def forward(self, x, past, past_len):
+        B, T, C = x.shape
+        H = self.config.num_heads
+        q, k, v = self.qkv(x).view(B, T, 3, H, C // H).transpose(1, 3).unbind(dim=2)  # B, H, T, C // H
+        if past is not None:  # write KV into the buffer
+            assert past_len == 0 or T == 1  # since is_causal has to be disabled
+            past[:, :, 0, past_len:past_len+T], past[:, :, 1, past_len:past_len+T] = k, v
+            k, v = past[:, :, 0, :past_len+T], past[:, :, 1, :past_len+T]
 
-    x = F.scaled_dot_product_attention(q, k, v, attn_mask=None, is_causal=T > 1, dropout_p=self.config.dropout if self.training else 0)
-    x = x.transpose(1, 2).contiguous().view(B, T, C)  # join the heads together
-    x = self.proj(x)
-    x = self.dropout(x)
-    return x
+        x = F.scaled_dot_product_attention(q, k, v, attn_mask=None, is_causal=T > 1, dropout_p=self.config.dropout if self.training else 0)
+        x = x.transpose(1, 2).contiguous().view(B, T, C)  # join the heads together
+        x = self.proj(x)
+        x = self.dropout(x)
+        return x
 
 class MLP(nn.Module):
-  def __init__(self, config):
-    super().__init__()
-    self.fc1 = nn.Linear(config.embed_size, config.embed_size*4, bias=config.bias)
-    self.fc2 = nn.Linear(config.embed_size*4, config.embed_size, bias=config.bias)
-    self.dropout = nn.Dropout(config.dropout)
+    def __init__(self, config):
+        super().__init__()
+        self.fc1 = nn.Linear(config.embed_size, config.embed_size*4, bias=config.bias)
+        self.fc2 = nn.Linear(config.embed_size*4, config.embed_size, bias=config.bias)
+        self.dropout = nn.Dropout(config.dropout)
 
-  def forward(self, x):
-    x = self.fc1(x)
-    x = self.fc2(F.gelu(x, approximate='tanh'))
-    x = self.dropout(x)
-    return x
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.fc2(F.gelu(x, approximate='tanh'))
+        x = self.dropout(x)
+        return x
 
 class Block(nn.Module):
-  def __init__(self, config):
-    super().__init__()
-    self.attn = Attention(config)
-    self.mlp = MLP(config)
-    self.ln1 = LayerNorm(config.embed_size, bias=config.bias)
-    self.ln2 = LayerNorm(config.embed_size, bias=config.bias)
+    def __init__(self, config):
+        super().__init__()
+        self.attn = Attention(config)
+        self.mlp = MLP(config)
+        self.ln1 = LayerNorm(config.embed_size, bias=config.bias)
+        self.ln2 = LayerNorm(config.embed_size, bias=config.bias)
 
-  def forward(self, x, past, past_len):
-    x = x + self.attn(self.ln1(x), past, past_len)
-    x = x + self.mlp(self.ln2(x))
-    return x
+    def forward(self, x, past, past_len):
+        x = x + self.attn(self.ln1(x), past, past_len)
+        x = x + self.mlp(self.ln2(x))
+        return x
 
 class GPT(nn.Module):
-  def __init__(self, config):
-    super().__init__()
-    self.config = config
-    self.embed_tokens = nn.Embedding(config.vocab_size, config.embed_size)
-    self.embed_pos = nn.Embedding(config.context_size, config.embed_size)
-    self.blocks = nn.ModuleList([Block(config) for _ in range(config.num_layers)])
-    self.dropout = nn.Dropout(config.dropout)
-    self.ln = LayerNorm(config.embed_size, bias=config.bias)
-    self.fc_out = nn.Linear(config.embed_size, config.vocab_size, bias=False)
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.embed_tokens = nn.Embedding(config.vocab_size, config.embed_size)
+        self.embed_pos = nn.Embedding(config.context_size, config.embed_size)
+        self.blocks = nn.ModuleList([Block(config) for _ in range(config.num_layers)])
+        self.dropout = nn.Dropout(config.dropout)
+        self.ln = LayerNorm(config.embed_size, bias=config.bias)
+        self.fc_out = nn.Linear(config.embed_size, config.vocab_size, bias=False)
 
-  def forward(self, x, past=None, past_len=0):
-    assert past is None or past_len < past.shape[4]
-    pos = torch.arange(past_len, past_len+x.shape[-1], dtype=torch.int32, device=x.device)
-    x = self.embed_tokens(x) + self.embed_pos(pos)
-    x = self.dropout(x)
-    for i, block in enumerate(self.blocks):
-      x = block(x, past[:,i] if past is not None else None, past_len)
-    x = self.fc_out(self.ln(x))
-    return x
+    def forward(self, x, past=None, past_len=0):
+        assert past is None or past_len < past.shape[4]
+        pos = torch.arange(past_len, past_len+x.shape[-1], dtype=torch.int32, device=x.device)
+        x = self.embed_tokens(x) + self.embed_pos(pos)
+        x = self.dropout(x)
+        for i, block in enumerate(self.blocks):
+            x = block(x, past[:,i] if past is not None else None, past_len)
+        x = self.fc_out(self.ln(x))
+        return x
 
-  def loss(self, logits, targets):
-    return F.cross_entropy(logits.flatten(end_dim=-1), targets, ignore_index=-1)
+    def loss(self, logits, targets):
+        return F.cross_entropy(logits.flatten(end_dim=-1), targets, ignore_index=-1)
 
-  @torch.no_grad()
-  def generate(self, context, num_tokens=1, temperature=1.0, top_k=-1):
-    past = torch.zeros(len(context), self.config.num_layers, self.config.num_heads, 2, self.config.context_size, self.config.embed_size // self.config.num_heads)
-    self(context, past, 0)  # init the kv cache
-    for i in range(num_tokens-1):
-      logits = self(context[:,-1:], past, context.shape[1]+i-1)[:, -1] / temperature
-      if top_k > 0:
-        v, _ = torch.topk(logits, top_k)
-        logits[logits < v[:, -1:]] = -torch.inf
-      probs = F.softmax(logits, dim=-1)
-      next_token = torch.multinomial(probs, num_samples=1)
-      context = torch.cat([context, next_token], dim=-1)
-    return context
+    @torch.no_grad()
+    def generate(self, context, num_tokens=1, temperature=1.0, top_k=-1):
+        past = torch.zeros(len(context), self.config.num_layers, self.config.num_heads, 2, self.config.context_size, self.config.embed_size // self.config.num_heads)
+        self(context, past, 0)  # init the kv cache
+        for i in range(num_tokens-1):
+            logits = self(context[:,-1:], past, context.shape[1]+i-1)[:, -1] / temperature
+            if top_k > 0:
+                v, _ = torch.topk(logits, top_k)
+                logits[logits < v[:, -1:]] = -torch.inf
+            probs = F.softmax(logits, dim=-1)
+            next_token = torch.multinomial(probs, num_samples=1)
+            context = torch.cat([context, next_token], dim=-1)
+        return context
